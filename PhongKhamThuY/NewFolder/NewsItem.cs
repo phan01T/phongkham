42e9b4ce92d8700602749ed281dceeb65844776c
phan01T <phan0101xm@gmail.com>
2024-11-02 17:15:08 +0700

    init project

---
+++ PhongKhamThuY/NewFolder/NewsItem.cs
@@ -0,0 +1,29 @@
+﻿namespace phongkhamthuy.Layout
+{
+    public class NewsItem
+    {
+        public string GetTitle()
+        {
+            return Title;
+        }
+
+        public void SetTitle(string value)
+        {
+            Title = value;
+        }
+
+        private string? url;
+
+        public required string Title { get; set; }
+
+        public string GetUrl()
+        {
+            return url;
+        }
+
+        public void SetUrl(string value)
+        {
+            url = value;
+        }
+    }
+}
